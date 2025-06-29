f8effc73ed16357a05c1e2e09f125634cb173efc
Mauricio <mauricio_cy@hotmail.com>
2025-06-29 18:52:24 -0300

    upload-widget main components

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,9 @@
+import { UploadWidget } from "./components/upload-widget";
+
 export function App() {
-  return <div>Hello World</div>;
+  return (
+    <main className="h-dvh flex flex-col items-center justify-center p-10">
+      <UploadWidget />
+    </main>
+  );
 }
