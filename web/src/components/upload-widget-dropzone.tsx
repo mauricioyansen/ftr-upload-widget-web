f1e96ca9b7813c7b55979029eb0181fb716be6d0
Mauricio <mauricio_cy@hotmail.com>
2025-06-29 20:39:48 -0300

    animations

--- web/src/components/upload-widget-dropzone.tsx
+++ web/src/components/upload-widget-dropzone.tsx
@@ -1,6 +1,11 @@
 import { useDropzone } from "react-dropzone";
+import CircularProgressBar from "./ui/circular-progress-bar";
+import { motion } from "motion/react";
 
 export function UploadWidgetDropzone() {
+  const isThereAnyPendingUpload = true;
+  const uploadGlobalPercentage = 66;
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: true,
     accept: {
@@ -13,7 +18,12 @@ export function UploadWidgetDropzone() {
   });
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+    <motion.div
+      className="px-3 flex flex-col gap-3"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2 }}
+    >
       <div
         data-active={isDragActive}
         className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500 data-[active=true]:text-indigo-400"
@@ -21,13 +31,26 @@ export function UploadWidgetDropzone() {
       >
         <input type="file" {...getInputProps()} />
 
-        <span className="text-xs">Drop your files here or</span>
-        <span className="text-xs underline">click to open picker</span>
+        {isThereAnyPendingUpload ? (
+          <div className="flex flex-col gap-2.5 items-center">
+            <CircularProgressBar
+              progress={uploadGlobalPercentage}
+              size={56}
+              strokeWidth={4}
+            />
+            <span className="text-xs">Uploading 2 files...</span>
+          </div>
+        ) : (
+          <>
+            <span className="text-xs">Drop your files here or</span>
+            <span className="text-xs underline">click to open picker</span>
+          </>
+        )}
       </div>
 
-      <span className="text-xs text-zinc-400">
+      <span className="text-xxs text-zinc-400">
         Only PNG and JPG files are supported.
       </span>
-    </div>
+    </motion.div>
   );
 }
